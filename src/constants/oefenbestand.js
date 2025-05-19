165cd809a64225928f6775a7633d019ede701ab5
Zeverouis <Zeverouis@gmail.com>
2025-05-19 15:39:01 +0200

    Added opdracht 2 + deel van opdracht 3

---
+++ src/constants/oefenbestand.js
@@ -0,0 +1,9 @@
+import { inventory } from './inventory.js';
+
+function showOutcomeInConsole() {
+    console.log('uitkomst opdracht 1a');
+    console.log('uitkomst opdracht 1b);
+}
+
+export default showOutcomeInConsole;
+
